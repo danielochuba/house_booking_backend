55360e1276a9c85b1069c242e06ae57fffbf667d
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-29 18:12:51 +0200

    Fixed rubocop linter errors

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::AuthenticationController < ApplicationController
 
     if user&.authenticate(params[:password])
       token = encode_token(user.id)
-      render json: { token: token }
+      render json: { token: }
     else
-      render json: { error: "Invalid username or password" }, status: :unauthorized
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
     end
   end
 
@@ -17,18 +17,18 @@ class Api::V1::AuthenticationController < ApplicationController
 
     if user.save
       token = encode_token(user.id)
-      render json: { token: token }, status: :created
+      render json: { token: }, status: :created
     else
-      render json: { error: user.errors.full_messages.join(", ") }, status: :unprocessable_entity
+      render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
     end
   end
 
   private
 
   def encode_token(user_id)
-    payload = { user_id: user_id }
+    payload = { user_id: }
     token = JWT.encode(payload, Rails.application.secrets.secret_key_base)
-    response.headers["Authorization"] = "Bearer #{token}"
+    response.headers['Authorization'] = "Bearer #{token}"
     token
   end
 end
