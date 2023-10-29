6eeeae8896413d828e375d251a061ec3d92f5805
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-29 17:51:30 +0200

    configure the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        post "/auth/login", to: "authentication#login"
+        post "/auth/register", to: "authentication#register"
+      end
+    end
+  end
 end
