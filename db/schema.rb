31d17d7cbb959e5c25ffa8c3c297bdcad12fb517
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-30 22:19:05 +0200

    Implement migration for house table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_153704) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_200244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "description", null: false
+    t.bigint "user_id", null: false
+    t.integer "price_per_day", null: false
+    t.string "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_houses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -22,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_153704) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "houses", "users"
 end
