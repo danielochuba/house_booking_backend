28956421a71ada02486c4b1408f2342902ad2ca5
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-30 19:11:10 +0200

    Implement authenticate user to make sure user is authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def authenticate
+    token = cookies[:token]
+    if token
+      decoded_token = decode_token(token)
+      user_id = decoded_token[0]["user_id"]
+      @current_user = User.find_by(id: user_id)
+    end
+  end
 end
