4650953380390a46dfe48ae436494e59c2829720
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-30 22:20:19 +0200

    Implement house conrtoller to create a new house for the logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     return unless token
 
     decoded_token = decode_token(token)
-    user_id = decoded_token[0]['user_id']
+    user_id = decoded_token[0]["user_id"]
     @current_user = User.find_by(id: user_id)
   end
 
