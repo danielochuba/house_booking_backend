2cc9aac0c882aa1fdcaf5fd473344217cd1c83b6
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-30 22:19:40 +0200

    configure the routes to include create house route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    namespace :api do
-      namespace :v1 do
-        post "/auth/login", to: "authentication#login"
-        post "/auth/register", to: "authentication#register"
-      end
+  namespace :api do
+    namespace :v1 do
+      post "/auth/login", to: "authentication#login"
+      post "/auth/register", to: "authentication#register"
+
+      resources :houses, only: [:create]
     end
   end
 end
