0e666d74b43ca91682c4150ff67dc0ca3ec7c767
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-30 21:46:38 +0200

    Fixed rubocop linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,14 @@
 class ApplicationController < ActionController::Base
   def authenticate
     token = cookies[:token]
-    if token
-      decoded_token = decode_token(token)
-      user_id = decoded_token[0]["user_id"]
-      @current_user = User.find_by(id: user_id)
-    end
+    return unless token
+
+    decoded_token = decode_token(token)
+    user_id = decoded_token[0]['user_id']
+    @current_user = User.find_by(id: user_id)
   end
 
-  def current_user
-    @current_user
-  end
+  attr_reader :current_user
 
   def decode_token(token)
     JWT.decode(token, Rails.application.secrets.secret_key_base)
