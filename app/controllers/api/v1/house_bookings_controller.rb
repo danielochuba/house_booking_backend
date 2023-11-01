c44c97d84360d183a245235827328da1e3136cca
Daniel Ochuba <danielochuba78@gmail.com>
2023-11-01 10:23:51 +0100

    add before actions

--- app/controllers/api/v1/house_bookings_controller.rb
+++ app/controllers/api/v1/house_bookings_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::HouseBookingsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate
   def create
     @booking = HouseBooking.new(booking_params)
 
