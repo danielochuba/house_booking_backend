c71072b3bbf5a0c507713f4d6a69c14fbd50ce93
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-11-01 17:43:18 +0200

    Fixed a bug with a token doesn't exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   def authenticate
-    token = request.headers['Authorization'].split[1]
+    token = request.headers["Authorization"]
     return unless token
 
+    token = token.split(" ")[1]
+
     decoded_token = decode_token(token)
-    user_id = decoded_token[0]['user_id']
+    user_id = decoded_token[0]["user_id"]
     @current_user = User.find_by(id: user_id)
   end
 
