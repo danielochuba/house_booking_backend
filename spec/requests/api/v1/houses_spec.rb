861224ba6d12f14b9e6c4348a33a947bf2f3c046
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-11-06 22:21:17 +0200

    Added create house and list all houses

--- spec/requests/api/v1/houses_spec.rb
+++ spec/requests/api/v1/houses_spec.rb
@@ -1,122 +1,66 @@
-require 'swagger_helper'
+require "swagger_helper"
 
-RSpec.describe 'api/v1/houses', type: :request do
+RSpec.describe "api/v1/houses", type: :request do
+  path "/api/v1/houses" do
+    get("list houses") do
+      tags "Houses"
+      produces "application/json"
+      security [Bearer: []]
 
-  path '/api/v1/houses/find_by_name' do
-
-    get('find_by_name house') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
+      response(200, "successful") do
+        schema type: :array,
+          items: {
+            type: :object,
+            properties: {
+              id: { type: :integer },
+              name: { type: :string },
+              address: { type: :string },
+              description: { type: :string },
+              price_per_day: { type: :integer },
+              image: { type: :string },
+            },
           }
-        end
-        run_test!
-      end
-    end
-  end
 
-  path '/api/v1/houses/{id}/delete_houses' do
-    # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
-
-    get('delete_houses house') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
-  path '/api/v1/houses' do
-
-    get('list houses') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
         run_test!
       end
     end
 
-    post('create house') do
-      response(200, 'successful') do
+    post("create house") do
+      tags "Houses"
+      consumes "application/json"
+      security [Bearer: []]
+      parameter name: :house, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          address: { type: :string },
+          description: { type: :string },
+          price_per_day: { type: :integer },
+          image: { type: :string },
+        },
+        required: %w[name address description price_per_day image],
+      }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
+      response(201, "successful") do
+        schema type: :object,
+          properties: {
+            id: { type: :integer },
+            name: { type: :string },
+            address: { type: :string },
+            description: { type: :string },
+            price_per_day: { type: :integer },
+            image: { type: :string },
           }
-        end
+
         run_test!
       end
-    end
-  end
 
-  path '/api/v1/houses/{id}' do
-    # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
-
-    get('show house') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
+      response(422, "unprocessable entity") do
+        schema type: :object,
+          properties: {
+            error: { type: :string },
           }
-        end
-        run_test!
-      end
-    end
 
-    delete('delete house') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
-  path '/api/v1/delete_houses' do
-
-    get('delete_houses house') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
         run_test!
       end
     end
